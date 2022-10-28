5d53063bc5c671704142c0fe6e7d35e01cc507d6
Prakhyath Bhandary <prakhz16@gmail.com>
2022-10-28 21:52:35 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from re import L
 from setuptools import find_packages,setup
 from typing import List
 
@@ -13,7 +12,9 @@ def get_requirements()-> List:
 
         with open('requirements.txt', 'r') as f:
             lines = f.readlines()
-
+        requirement_list = [line.replace('\n', '') for line in lines]
+        if "-e ." in requirement_list:
+            requirement_list.remove("- e.")
 
         return requirement_list
     except Exception as e:
